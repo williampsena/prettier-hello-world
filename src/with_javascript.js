c9ca2af7c93acdcf46d6d8832b294b7ab9f7499c
Will <williampsena@gmail.com>
2023-03-22 09:43:29 -0300

    Prettier setup

---
+++ src/with_javascript.js
@@ -0,0 +1,9 @@
+export const printDoubleQuote = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+
+export function testPrintWidth ( x ) { return (x + 1) * (x +2) / (x + 3) + (x + 4 + 5 + 6)}
+
+export const testSemi = "ok";
+
+export function testArrowParens(x) { return (y) => x * y * 2 }
+
+export const testTrailingComma = [1, 2,]
---
+++ src/with_typescript.ts
@@ -0,0 +1,9 @@
+export const printDoubleQuote = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+
+export const testPrintWidth = ( x ) => (x + 1) * (x +2) / (x + 3) + (x + 4 + 5 + 6)
+
+export const testSemi = "ok";
+
+export const testArrowParens = (x) => (y) => x * y * 2
+
+export const testTrailingComma = [1, 2,]
